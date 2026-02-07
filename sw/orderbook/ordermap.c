5cd8f3f4ff80d7bd25bcb9553a41f3dd87565435
harristmai <harristmai@gmail.com>
2026-02-06 20:22:04 -0800

    Update ordermap functions to take little endian inputs

--- sw/orderbook/ordermap.c
+++ sw/orderbook/ordermap.c
@@ -1,6 +1,25 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+// Manual byte order conversions for Windows/MinGW compatibility
+static inline uint32_t ntohl_manual(uint32_t x) {
+    return ((x & 0xFF000000) >> 24) |
+           ((x & 0x00FF0000) >> 8)  |
+           ((x & 0x0000FF00) << 8)  |
+           ((x & 0x000000FF) << 24);
+}
+
+static inline uint64_t be64toh_manual(uint64_t x) {
+    return ((x & 0xFF00000000000000ULL) >> 56) |
+           ((x & 0x00FF000000000000ULL) >> 40) |
+           ((x & 0x0000FF0000000000ULL) >> 24) |
+           ((x & 0x000000FF00000000ULL) >> 8)  |
+           ((x & 0x00000000FF000000ULL) << 8)  |
+           ((x & 0x0000000000FF0000ULL) << 24) |
+           ((x & 0x000000000000FF00ULL) << 40) |
+           ((x & 0x00000000000000FFULL) << 56);
+}
+
 // ob_update packed into 64 bits:
 // Bit 63: side (0=buy, 1=sell)
 // Bits 32-62: price >> 2 (31 bits, divide by 4 using fast bit shift)
@@ -17,11 +36,17 @@ typedef struct {
 
 #define MAX_ORDERS 964236
 
-static order_entry_t order_map[MAX_ORDERS];
+extern order_entry_t order_map[MAX_ORDERS];
+order_entry_t order_map[MAX_ORDERS];
 
 ob_update order_add(uint64_t id, uint32_t price,
                     uint32_t qty, uint8_t side)
 {
+    // Convert from network byte order (big-endian) to host byte order
+    id = be64toh_manual(id);
+    price = ntohl_manual(price);
+    qty = ntohl_manual(qty);
+    
     order_entry_t *e = &order_map[id];
     e->price = price;
     e->qty   = qty;
@@ -34,6 +59,10 @@ ob_update order_add(uint64_t id, uint32_t price,
 
 ob_update order_cancel_execute(uint64_t id, uint32_t qty)
 {
+    // Convert from network byte order (big-endian) to host byte order
+    id = be64toh_manual(id);
+    qty = ntohl_manual(qty);
+    
     order_entry_t *e = &order_map[id];
     
     // Pack into 64 bits: [side(1bit)][price>>2(31bits)][qty(32bits)]
