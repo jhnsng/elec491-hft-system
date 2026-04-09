b74be6b23b64e876e74474fd55b7faea7fd2e550
SofiyaSp <sofiyaspolitak12@gmail.com>
2026-04-08 18:21:31 -0700

    m

--- hw/algorithm/src/rtl/inc/algo_cfg_pkg.sv
+++ hw/algorithm/src/rtl/inc/algo_cfg_pkg.sv
@@ -6,7 +6,8 @@ package algo_cfg_pkg;
   // Perfect for catching short-term SPY momentum!
   localparam logic [15:0] ALPHA_FAST_Q16   = 16'd26214; 
   localparam logic [15:0] ALPHA_SLOW_Q16   = 16'd14563; 
-  localparam logic [15:0] ALPHA_SIGNAL_Q16 = 16'd32768; 
+  //localparam logic [15:0] ALPHA_SIGNAL_Q16 = 16'd32768; 
+  localparam logic [15:0] ALPHA_SIGNAL_Q16  = 16'd6553;  // 0.10 (Slowed down from 0.50)
 
   // 5 ticks = $0.05 max spread. SPY is highly liquid, so this safely 
   // prevents you from trading during crazy volatility spikes.
@@ -16,7 +17,7 @@ package algo_cfg_pkg;
   // CHANGE THIS TO 3 or 5!
   // Since 1 unit = $0.01, a threshold of 3 means the momentum must 
   // clearly break out by 3 full cents before the FPGA pulls the trigger.
-  localparam logic signed [63:0] CROSS_THRESHOLD = 64'd5; 
+  localparam logic signed [63:0] CROSS_THRESHOLD = 64'd3; 
 
   
 
